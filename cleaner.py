4b44ad9a6e88323a18d93cf6476bfe0c5460b4d8
Shiori Hironaka <noahluces@gmail.com>
2023-01-30 18:58:07 +0900

    Reformatted

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 """
 BibTexのいらない項目を削除するスクリプト
 """
@@ -13,99 +12,220 @@ from bibtexparser.bibdatabase import BibDatabase
 from bibtexparser.bwriter import BibTexWriter
 
 
-class Setting():
-    items = ['savetitlecase', 'replaceid', 'jauthor', 'revjauthor']
+class Setting:
+    items = ["savetitlecase", "replaceid", "jauthor", "revjauthor"]
 
 
 # from Wikipedia (https://ja.wikipedia.org/wiki/BibTeX)
 necessary = {
-    #'article': ['author', 'title', 'journal', 'year', 'volume', 'number', 'pages', 'month', 'note', 'key'],
-    'article': ['author', 'title', 'journal', 'year', 'volume', 'number', 'pages', 'key'],
-    'phdthesis': ['author', 'title', 'school', 'year', 'address', 'month', 'note', 'key'],
-    'masterthesis': ['author', 'title', 'school', 'year', 'address', 'month', 'note', 'key'],
-    'proceedings': ['title', 'year', 'editor', 'publisher', 'organization', 'address', 'month', 'note', 'key', 'volume', 'number'],
-#    'inproceedings': ['author', 'title', 'booktitle', 'year', 'editor', 'pages', 'organization', 'publisher', 'address', 'month', 'note', 'key'],
-    'inproceedings': ['author', 'title', 'booktitle', 'year', 'pages', 'key'],
-    'conference': ['author', 'title', 'booktitle', 'year', 'editor', 'pages', 'organization', 'publisher', 'address', 'month', 'note', 'key'],
-    'book': ['author', 'editor', 'title', 'publisher', 'year', 'volume', 'series', 'address', 'edition', 'month', 'note', 'key'],
-    'booklet': ['title', 'author', 'howpublished', 'address', 'month', 'year', 'note', 'key'],
-    'inbook': ['author', 'editor', 'title', 'chapter', 'pages', 'publisher', 'year', 'volume', 'series', 'address', 'edition', 'month', 'note', 'key'],
-    'incollection': ['author', 'title', 'booktitle', 'year', 'editor', 'pages', 'organization', 'publisher', 'address', 'month', 'note', 'key'],
-    'manual': ['title', 'author', 'organization', 'address', 'edition', 'month', 'year', 'note', 'key'],
-    'techreport': ['author', 'title', 'institution', 'year', 'type', 'number', 'address', 'month', 'note', 'key'],
-    'misc': ['author', 'title', 'howpublished', 'month', 'year', 'note', 'key'],
-    'unpublished': ['author', 'title', 'note', 'month', 'year', 'key']
+    "article": [
+        "author",
+        "title",
+        "journal",
+        "year",
+        "volume",
+        "number",
+        "pages",
+        "key",
+    ],
+    "phdthesis": [
+        "author",
+        "title",
+        "school",
+        "year",
+        "address",
+        "month",
+        "note",
+        "key",
+    ],
+    "masterthesis": [
+        "author",
+        "title",
+        "school",
+        "year",
+        "address",
+        "month",
+        "note",
+        "key",
+    ],
+    "proceedings": [
+        "title",
+        "year",
+        "editor",
+        "publisher",
+        "organization",
+        "address",
+        "month",
+        "note",
+        "key",
+        "volume",
+        "number",
+    ],
+    "inproceedings": ["author", "title", "booktitle", "year", "pages", "key"],
+    "conference": [
+        "author",
+        "title",
+        "booktitle",
+        "year",
+        "editor",
+        "pages",
+        "organization",
+        "publisher",
+        "address",
+        "month",
+        "note",
+        "key",
+    ],
+    "book": [
+        "author",
+        "editor",
+        "title",
+        "publisher",
+        "year",
+        "volume",
+        "series",
+        "address",
+        "edition",
+        "month",
+        "note",
+        "key",
+    ],
+    "booklet": [
+        "title",
+        "author",
+        "howpublished",
+        "address",
+        "month",
+        "year",
+        "note",
+        "key",
+    ],
+    "inbook": [
+        "author",
+        "editor",
+        "title",
+        "chapter",
+        "pages",
+        "publisher",
+        "year",
+        "volume",
+        "series",
+        "address",
+        "edition",
+        "month",
+        "note",
+        "key",
+    ],
+    "incollection": [
+        "author",
+        "title",
+        "booktitle",
+        "year",
+        "editor",
+        "pages",
+        "organization",
+        "publisher",
+        "address",
+        "month",
+        "note",
+        "key",
+    ],
+    "manual": [
+        "title",
+        "author",
+        "organization",
+        "address",
+        "edition",
+        "month",
+        "year",
+        "note",
+        "key",
+    ],
+    "techreport": [
+        "author",
+        "title",
+        "institution",
+        "year",
+        "type",
+        "number",
+        "address",
+        "month",
+        "note",
+        "key",
+    ],
+    "misc": ["author", "title", "howpublished", "month", "year", "note", "key"],
+    "unpublished": ["author", "title", "note", "month", "year", "key"],
 }
+
 
 def print_necessary_as_html():
     fd = sys.stdout
-    print('<dl>', file=fd)
+    print("<dl>", file=fd)
     for key, value in necessary.items():
-        print('<dt>%s</dt>' % key, file=fd)
+        print("<dt>%s</dt>" % key, file=fd)
         for field in value:
-            print('<dd>%s</dd>' % field, file=fd)
-    print('</dl>', file=fd)
+            print("<dd>%s</dd>" % field, file=fd)
+    print("</dl>", file=fd)
 
 
 def is_japanese(string):
     for c in string:
         name = unicodedata.name(c)
-        if('CJK UNIFIED' in name or
-           'HIRAGANA' in name or
-           'KATAKANA' in name):
+        if "CJK UNIFIED" in name or "HIRAGANA" in name or "KATAKANA" in name:
             return True
     return False
 
 
 def make_id(entry):
     # authorが含まれていないエントリーはIDを変更しない
-    if 'author' not in entry or 'year' not in entry or not entry['author']:
-        return entry['ID']
-    name = entry['author'].split('and')[0].strip().split()[0].strip(',')
+    if "author" not in entry or "year" not in entry or not entry["author"]:
+        return entry["ID"]
+    name = entry["author"].split("and")[0].strip().split()[0].strip(",")
     # exclude spaces
-    name = name.replace(' ', '').replace('.', '').replace('{', '').replace('}', '')
+    name = name.replace(" ", "").replace(".", "").replace("{", "").replace("}", "")
 
-    return name + entry['year']
+    return name + entry["year"]
+
 
 def clean_entries(bib_database, option):
     cleaned = []
     for entry in bib_database.entries:
         # 必要なフィールドだけ取り出す
-        needs = necessary[entry['ENTRYTYPE']]
+        needs = necessary[entry["ENTRYTYPE"]]
         e = {}
-        e['ID'] = entry['ID']
-        e['ENTRYTYPE'] = entry['ENTRYTYPE']
+        e["ID"] = entry["ID"]
+        e["ENTRYTYPE"] = entry["ENTRYTYPE"]
         for item in entry.keys():
             if item in needs:
                 e[item] = entry[item]
 
         # 日本人ぽいauthorは姓名のあいだの,を消す
-        if option['jauthor'] and 'author' in e and is_japanese(e['author']):
+        if option["jauthor"] and "author" in e and is_japanese(e["author"]):
             names = []
-            for fullname in entry['author'].split('and'):
-                name = [name.strip() for name in fullname.split(',')]
-                if option['revjauthor']:
+            for fullname in entry["author"].split("and"):
+                name = [name.strip() for name in fullname.split(",")]
+                if option["revjauthor"]:
                     name = reversed(name)
-                names.append(' '.join(name))
-            e['author'] = ' and '.join(names)
+                names.append(" ".join(name))
+            e["author"] = " and ".join(names)
 
         # titleを{}でかこむ (caseを保存するため)
-        if option['savetitlecase'] and 'title' in entry:
-            e['title'] = '{%s}' % e['title']
+        if option["savetitlecase"] and "title" in entry:
+            e["title"] = "{%s}" % e["title"]
 
         # 引用keyをauthor+yearで置き換える
-        if option['replaceid']:
-            e['ID'] = make_id(e)
+        if option["replaceid"]:
+            e["ID"] = make_id(e)
         cleaned.append(e)
 
     # keyが同じentryをuniqueにする
-    counter = Counter([entry['ID'] for entry in cleaned])
+    counter = Counter([entry["ID"] for entry in cleaned])
     for key, count in counter.items():
         if count >= 2:
             cnt = 0
             for entry in cleaned:
-                if entry['ID'] == key:
-                    entry['ID'] = entry['ID'] + chr(ord('a') + cnt)
+                if entry["ID"] == key:
+                    entry["ID"] = entry["ID"] + chr(ord("a") + cnt)
                     cnt += 1
 
     # make new db
@@ -113,15 +233,27 @@ def clean_entries(bib_database, option):
     db.entries = cleaned
     return db
 
+
 def parse_args():
     import argparse
-    parser = argparse.ArgumentParser(description='Bibtex cleaner')
-    parser.add_argument('bibfile', type=argparse.FileType('r'),
-                        help='汚いbibファイル（入力ファイル名; default: stdin）',
-                        default=sys.stdin, nargs='?')
-    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
-                        help='きれいなbibファイル名（出力ファイル名; default: stdout）')
+
+    parser = argparse.ArgumentParser(description="Bibtex cleaner")
+    parser.add_argument(
+        "bibfile",
+        type=argparse.FileType("r"),
+        help="汚いbibファイル（入力ファイル名; default: stdin）",
+        default=sys.stdin,
+        nargs="?",
+    )
+    parser.add_argument(
+        "outfile",
+        nargs="?",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="きれいなbibファイル名（出力ファイル名; default: stdout）",
+    )
     return parser.parse_args()
+
 
 def bibtex_cleaner(bibtext, option):
     try:
@@ -130,9 +262,10 @@ def bibtex_cleaner(bibtext, option):
         writer = BibTexWriter()
         return writer.write(cleaned_database)
     except Exception:
-        return 'Error. 入力形式はbibtexですか？（または変換プログラムのバグの可能性があります）\n'
+        return "Error. 入力形式はbibtexですか？（または変換プログラムのバグの可能性があります）\n"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_args()
     bibtex_str = args.bibfile.read()
     args.outfile.write(bibtex_cleaner(bibtex_str))
--- endpoint.py
+++ endpoint.py
@@ -6,17 +6,18 @@ from flask import Flask, render_template, request, Response, abort, render_templ
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/clean', methods=['POST'])
+
+@app.route("/clean", methods=["POST"])
 def clean():
-    if request.method == 'POST':
-        if 'fromtext' in request.form and 'bibtext' in request.form:
-            bibtext = request.form['bibtext']
-        elif 'fromfile' in request.form and 'bibfile' in request.files:
-            bibtext = request.files['bibfile'].read().decode('utf-8')
+    if request.method == "POST":
+        if "fromtext" in request.form and "bibtext" in request.form:
+            bibtext = request.form["bibtext"]
+        elif "fromfile" in request.form and "bibfile" in request.files:
+            bibtext = request.files["bibfile"].read().decode("utf-8")
         else:
             # error
             abort(400)
@@ -24,20 +25,21 @@ def clean():
         # エラー処理
         if bibtext is None:
             abort(400)
-        if bibtext == '':
-            return Response('Error. 入力が空です', mimetype='text/plain')
+        if bibtext == "":
+            return Response("Error. 入力が空です", mimetype="text/plain")
 
         # 設定を取り出す
         option = {}
         for opt in Setting.items:
-            if request.form.get(opt) == 'on':
+            if request.form.get(opt) == "on":
                 option[opt] = True
             else:
                 option[opt] = False
 
         cleaned = bibtex_cleaner(bibtext, option)
-        return Response(cleaned, mimetype='text/plain')
+        return Response(cleaned, mimetype="text/plain")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.debug = True
     app.run()
